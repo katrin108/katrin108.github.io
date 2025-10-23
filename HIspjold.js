a96c6d52d5088a6c1980a6227f88a1c2cd0d90bb
katrin108 <kas108@hi.is>
2025-10-23 11:43:08 +0000

    V6

---
+++ HIspjold.js
@@ -0,0 +1,184 @@
+/////////////////////////////////////////////////////////////////
+//    S�nid�mi � T�lvugraf�k
+//     S�nid�mi um endurtekin mynstur.  Tv�v�tt spjald
+//     skilgreint og varpa� � �a� mynd sem er lesin inn.
+//     H�gt a� sn�a spjaldinu og f�ra til.
+//
+//    Hj�lmt�r Hafsteinsson, okt�ber 2025
+/////////////////////////////////////////////////////////////////
+var canvas;
+var gl;
+
+var numVertices  = 6;
+
+var program;
+
+var pointsArray = [];
+var texCoordsArray = [];
+
+var texture;
+
+var movement = false;
+var spinX = 0;
+var spinY = 0;
+var origX;
+var origY;
+
+var zDist = 5.0;
+
+var proLoc;
+var mvLoc;
+
+//    4-------3  2
+//    |     /  / |
+//    |   /  /   |       
+//    | /  /     |
+//    5  0-------1
+//
+// Tveir �r�hyrningar sem mynda spjald � z=0 planinu
+var vertices = [
+    vec4( 0.0, 0.0, 0.0, 1.0 ),      // ne�ri vinstri
+    vec4(  0.0, 1.0, 0.0, 1.0 ),      // ne�ri h�gri
+    vec4(  1.0,  1.0, 0.0, 1.0 ),      // efri h�gri
+    vec4(  1.0,  1.0, 0.0, 1.0 ),  
+    vec4( 0.0,  1.0, 0.0, 1.0 ),
+    vec4( 0.0, 0.0, 0.0, 1.0 )
+];
+
+// Mynsturhnit fyrir spjaldi�
+var texCoords = [
+    vec2( 0.0, 0.0 ),
+    vec2( 1.0, 0.0 ),
+    vec2( 1.0, 1.0 ),
+    vec2( 1.0, 1.0 ),
+    vec2( 0.0, 1.0 ),
+    vec2( 0.0, 0.0 )
+];
+
+
+function configureTexture( image ) {
+    texture = gl.createTexture();
+    gl.bindTexture( gl.TEXTURE_2D, texture );
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image );
+    gl.generateMipmap( gl.TEXTURE_2D );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT );
+//    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT );
+//    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT );
+//    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE );
+//    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
+    
+    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+}
+
+
+window.onload = function init() {
+
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.9, 1.0, 1.0, 1.0 );
+    
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+    
+    var tBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoords), gl.STATIC_DRAW );
+    
+    var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
+    gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vTexCoord );
+
+    // N� � mynstur �r html-skr�:
+    var image = document.getElementById("texImage");
+    configureTexture( image );
+
+
+    proLoc = gl.getUniformLocation( program, "projection" );
+    mvLoc = gl.getUniformLocation( program, "modelview" );
+
+    var proj = perspective( 50.0, 1.0, 0.2, 100.0 );
+    gl.uniformMatrix4fv(proLoc, false, flatten(proj));
+    
+
+    //event listeners for mouse
+    canvas.addEventListener("mousedown", function(e){
+        movement = true;
+        origX = e.clientX;
+        origY = e.clientY;
+        e.preventDefault();         // Disable drag and drop
+    } );
+
+    canvas.addEventListener("mouseup", function(e){
+        movement = false;
+    } );
+
+    canvas.addEventListener("mousemove", function(e){
+        if(movement) {
+    	    spinY = ( spinY + (e.clientX - origX) ) % 360;
+            spinX = ( spinX + (e.clientY - origY) ) % 360;
+            origX = e.clientX;
+            origY = e.clientY;
+        }
+    } );
+    
+    // Event listener for keyboard
+     window.addEventListener("keydown", function(e){
+         switch( e.keyCode ) {
+            case 38:	// upp �r
+                zDist += 0.1;
+                break;
+            case 40:	// ni�ur �r
+                zDist -= 0.1;
+                break;
+         }
+     }  );  
+
+    // Event listener for mousewheel
+     window.addEventListener("wheel", function(e){
+         if( e.deltaY > 0.0 ) {
+             zDist += 0.2;
+         } else {
+             zDist -= 0.2;
+         }
+     }  );  
+       
+       
+    render();
+ 
+}
+
+var render = function(){
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // sta�setja �horfanda og me�h�ndla m�sarhreyfingu
+    var mv = lookAt( vec3(0.0, 0.0, zDist), vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0) );
+    mv = mult( mv, rotateX( spinX ) );
+    mv = mult( mv, rotateY( spinY ) );
+    
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+
+    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+
+    requestAnimFrame(render);
+}
