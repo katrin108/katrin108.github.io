23c7f04c57f26a4aa8d2444fc094cf0de8018e67
katrin108 <kas108@hi.is>
2025-10-23 12:22:53 +0000

    V6

--- HIspjold.js
+++ HIspjold.js
@@ -58,6 +58,7 @@ var texCoords = [
     vec2( -1.0, 0.0 )//0
 ];
 */
+/*
 var texCoords = [
     vec2( 0.0, 1.0 ),//0
     vec2( 2.5, 1.0 ),//1
@@ -65,6 +66,14 @@ var texCoords = [
     vec2( 2.5, 0.0 ),//2
     vec2( 0.0,0.0 ),//3
     vec2( 0.0, 1.0 )//0
+];*/
+var texCoords = [
+    vec2( 1.0, 3.0 ),//0
+    vec2( 0.0, 3.0 ),//1
+    vec2( 0.0, 0.0 ),//2
+    vec2( 0.0, 0.0 ),//2
+    vec2( 1.0,0.0 ),//3
+    vec2( 1.0, 3.0 )//0
 ];
 
 function configureTexture( image ) {
