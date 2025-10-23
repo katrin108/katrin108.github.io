c871c95dd767c2beabcde0ffe692de8dc1e0509d
katrin108 <kas108@hi.is>
2025-10-23 12:58:31 +0000

    V6

--- HIspjold.js
+++ HIspjold.js
@@ -58,7 +58,7 @@ var texCoords = [
     vec2( -1.0, 0.0 )//0
 ];
 
-*//*
+*/
 var texCoords = [
     vec2( 0.0, 1.0 ),//0
     vec2( 2.5, 1.0 ),//1
@@ -66,7 +66,7 @@ var texCoords = [
     vec2( 2.5, 0.0 ),//2
     vec2( 0.0,0.0 ),//3
     vec2( 0.0, 1.0 )//0
-];
+];/*
 var texCoords = [
     vec2( 1.0, 3.0 ),//0
     vec2( 0.0, 3.0 ),//1
